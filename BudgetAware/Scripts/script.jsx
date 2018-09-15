a3ac09eaf00622aa9b1cf58d7a30003cf046d4d7
Luke Walz <lukewalz1@gmail.com>
2018-09-14 23:24:56 -0500

    got it

--- BudgetAware/Scripts/script.jsx
+++ BudgetAware/Scripts/script.jsx
@@ -1,29 +1,45 @@
-﻿class PurchaseSummary extends React.Component {
+﻿class Company extends React.Component {
     constructor(props) {
         super(props);
-        this.deserialize = this.deserialize.bind(this);
-        this.componentDidMount = this.componentDidMount.bind(this);
-        this.state = {
-            purchases: []
-        }
-    }
-    componentDidMount() {
-        let purchasesText = this.deserialize(document.getElementById('ContentPlaceHolder1_hiddenJsonPurchases'));
-    }
 
-    deserialize(text) {
-        let value = JSON.parse(text.innerText);
-
-        this.setState({
-            purchases: value
-        })
     }
 
     render() {
         return (
-            <p>{this.state.purchases.Company}</p>
+            <div>
+                <div className='row'>
+                    Company: {this.props.name}
+                </div>
+            </div>
         )
     }
+}
+
+class PurchaseSummary extends React.Component {
+    constructor(props) {
+        super(props);
+        this.deserialize = this.deserialize.bind(this);
+        this.state = {
+            purchases: []
+        }
+    }
+
+    deserialize() {
+        let text = document.getElementById('ContentPlaceHolder1_hiddenJsonPurchases')
+        let value = JSON.parse(text.innerText);
+        let purchaseArray = Array.from(value);
+        purchaseArray.forEach(i => {
+            this.state.purchases.push(i.Company)
+        });
+        console.log(this.state.purchases)
+    }
+
+    render() {
+        this.deserialize();
+        return (this.state.purchases.map(e => <div key={e}><Company name={e} /></div>))
+    }
+
+
 }
 
 ReactDOM.render(<PurchaseSummary />, document.getElementById('app'))

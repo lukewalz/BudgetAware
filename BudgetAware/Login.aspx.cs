7dc0b39365012712f1556a05cea95504be590a24
Luke Walz <Luke_Walz@gallup.com>
2018-09-17 17:31:26 -0500

    sdfsd

--- BudgetAware/Login.aspx.cs
+++ BudgetAware/Login.aspx.cs
@@ -74,7 +74,6 @@ namespace BudgetAware
                     }
                     else
                     {
-                        throw new Exception("Invalid Password");
                     }
                 }
                 else
